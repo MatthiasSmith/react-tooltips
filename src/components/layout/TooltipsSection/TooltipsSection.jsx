44aed3016dbe6883b3e1b624ea97512543bd24c6
Matthew Smith <musicsmith4@gmail.com>
2020-01-07 22:00:36 -0500

    add .gitignore, update packages, refactor layout into components

---
+++ src/components/layout/TooltipsSection/TooltipsSection.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { TooltipButton } from '../../common/TooltipButton/TooltipButton';
+
+export const TooltipsSection = ({ sectionTitle, subTitle, contentType, showOn, children }) => (
+  <section className="tooltips-container">
+    <h3 className="section-header">{sectionTitle}</h3>
+    <p className="sub-header-text">{subTitle}</p>
+    <div className="flex-row space-between">
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="top-right">
+        {children}
+      </TooltipButton>
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="top-center">
+        {children}
+      </TooltipButton>
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="top-left">
+        {children}
+      </TooltipButton>
+    </div>
+    <div className="flex-row space-between">
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="left">
+        {children}
+      </TooltipButton>
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="right">
+        {children}
+      </TooltipButton>
+    </div>
+    <div className="flex-row space-between">
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="bottom-right">
+        {children}
+      </TooltipButton>
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="bottom-center">
+        {children}
+      </TooltipButton>
+      <TooltipButton
+        showOn={showOn}
+        contentType={contentType}
+        forcePosition="bottom-left">
+        {children}
+      </TooltipButton>
+    </div>
+  </section>
+);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './styles.scss';
 import { TooltipButton } from './components/common/TooltipButton/TooltipButton';
 import { ConfirmBox } from './components/common/ConfirmBox/ConfirmBox';
 import { ActionsBox } from './components/common/ActionsBox/ActionsBox';
+import { TooltipsSection } from './components/layout/TooltipsSection/TooltipsSection';
 
 const loremIpsumText = (
   <p>
@@ -18,208 +19,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <section className="tooltips-container">
-          <h3 className="section-header">Text Tooltips</h3>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="topLeftTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="top-right">
-              {loremIpsumText}
-            </TooltipButton>
-            <TooltipButton
-              id="topCenterTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="top-center">
-              {loremIpsumText}
-            </TooltipButton>
-            <TooltipButton
-              id="topRightTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="top-left">
-              {loremIpsumText}
-            </TooltipButton>
-          </div>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="middleLeftTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="left">
-              {loremIpsumText}
-            </TooltipButton>
-            <TooltipButton
-              id="middleRightTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="right">
-              {loremIpsumText}
-            </TooltipButton>
-          </div>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="bottomLeftTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="bottom-right">
-              {loremIpsumText}
-            </TooltipButton>
-            <TooltipButton
-              id="bottomCenterTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="bottom-center">
-              {loremIpsumText}
-            </TooltipButton>
-            <TooltipButton
-              id="bottomRightTextButton"
-              showOn="hover"
-              contentType="text"
-              forcePosition="bottom-left">
-              {loremIpsumText}
-            </TooltipButton>
-          </div>
-        </section>
-        <hr />
-        <section className="tooltips-container">
-          <h3 className="section-header">Action Box Tooltips</h3>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="topLeftActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="top-right">
-              <ActionsBox />
-            </TooltipButton>
-            <TooltipButton
-              id="topCenterActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="top-center">
-              <ActionsBox />
-            </TooltipButton>
-            <TooltipButton
-              id="topRightActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="top-left">
-              <ActionsBox />
-            </TooltipButton>
-          </div>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="middleLeftActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="left">
-              <ActionsBox />
-            </TooltipButton>
-            <TooltipButton
-              id="middleRightActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="right">
-              <ActionsBox />
-            </TooltipButton>
-          </div>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="bottomLeftActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="bottom-right">
-              <ActionsBox />
-            </TooltipButton>
-            <TooltipButton
-              id="bottomCenterActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="bottom-center">
-              <ActionsBox />
-            </TooltipButton>
-            <TooltipButton
-              id="bottomRightActionsButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="bottom-left">
-              <ActionsBox />
-            </TooltipButton>
-          </div>
-        </section>
-        <hr />
-        <section className="tooltips-container">
-          <h3 className="section-header">Confirm Box Tooltips</h3>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="topLeftConfirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="top-right">
-              <ConfirmBox />
-            </TooltipButton>
-            <TooltipButton
-              id="topCenterConfirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="top-center">
-              <ConfirmBox />
-            </TooltipButton>
-            <TooltipButton
-              id="topRightConfirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="top-left">
-              <ConfirmBox />
-            </TooltipButton>
-          </div>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="middleLeftConfirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="left">
-              <ConfirmBox />
-            </TooltipButton>
-            <TooltipButton
-              id="middleRightConfirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="right">
-              <ConfirmBox />
-            </TooltipButton>
-          </div>
-          <div className="flex-row space-between">
-            <TooltipButton
-              id="bottomLeftConfirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="bottom-right">
-              <ConfirmBox />
-            </TooltipButton>
-            <TooltipButton
-              id="bottomCenterCofirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="bottom-center">
-              <ConfirmBox />
-            </TooltipButton>
-            <TooltipButton
-              id="bottomRightCofirmButton"
-              showOn="click"
-              contentType="custom"
-              forcePosition="bottom-left">
-              <ConfirmBox />
-            </TooltipButton>
-          </div>
-        </section>
-        <hr />
-        <section className="tooltips-container" style={{ marginBottom: 200 }}>
           <h3 className="section-header">Dynamically Positioned Tooltips</h3>
           <p className="sub-header-text">
-            While tooltips above have static positioning in place, <br />
-            these tooltips change position based on the available space.
+            These tooltips change position based on the available space.
           </p>
           <div className="flex-row space-between">
             <TooltipButton id="dynamicPositionTextTooltip">
@@ -239,6 +41,30 @@ class App extends React.Component {
             </TooltipButton>
           </div>
         </section>
+        <hr />
+        <TooltipsSection
+          contentType="text"
+          showOn="hover"
+          sectionTitle="Text Tooltips"
+          subTitle="(statically posititoned)">
+          {loremIpsumText}
+        </TooltipsSection>
+        <hr />
+        <TooltipsSection
+          contentType="custom"
+          showOn="click"
+          sectionTitle="Action Box Tooltips"
+          subTitle="(statically posititoned)">
+          <ActionsBox />
+        </TooltipsSection>
+        <hr />
+        <TooltipsSection
+          contentType="custom"
+          showOn="click"
+          sectionTitle="Confirm Box Tooltips"
+          subTitle="(statically posititoned)">
+          <ConfirmBox />
+        </TooltipsSection>
       </div>
     );
   }
